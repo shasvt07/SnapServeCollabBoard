e98ec8f93fd5f9f7fe539de268084b6b3fc81594
shasvt07 <lit2021038@iiitl.ac.in>
2024-10-02 23:00:21 +0530

    bug-fix

--- app/App.tsx
+++ app/App.tsx
@@ -508,9 +508,14 @@ const Home = () => {
       />
 
       <section className='flex h-full flex-row'>
-        <LeftSidebar allShapes={Array.from(canvasObjects)} />
+        {/* <LeftSidebar allShapes={Array.from(canvasObjects)} /> */}
 
-        <Live canvasRef={canvasRef} undo={undo} redo={redo} />
+        <Live
+          fabricRef={fabricRef}
+          canvasRef={canvasRef}
+          undo={undo}
+          redo={redo}
+        />
 
         <RightSidebar
           elementAttributes={elementAttributes}
